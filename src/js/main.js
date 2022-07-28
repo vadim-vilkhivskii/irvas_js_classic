8c727abc017dc83c6703af9ec53c4ea8b4137aa4
Vadim Vilkhivskiy <vadim.v.valki@gmail.com>
2022-07-29 02:00:44 +0300

    added module with displaying images and timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,23 @@ import modals from './modules/modals';
 import tabs from  './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState'
+import timer from './modules/timer'
+import images from './modules/images'
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     let modalState = {};
-    changeModalState(modalState)
+    let deadline = '2022-08-01'
 
+
+    changeModalState(modalState)
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'Inline-block');
     forms(modalState);
+    timer('.container1', deadline);
+    images();
 });
 
